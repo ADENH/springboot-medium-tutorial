1c1c0245eac187043055f83046abac651d49139b
adenurhidayat <adenurhidayat101294@gmail.com>
2021-03-06 19:16:55 +0700

    create log file

--- src/main/java/com/demo/medium/config/aspect/target/PositionAspect.java
+++ src/main/java/com/demo/medium/config/aspect/target/PositionAspect.java
@@ -2,8 +2,6 @@ package com.demo.medium.config.aspect.target;
 
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,23 +9,26 @@ import org.springframework.stereotype.Component;
 
 import com.demo.medium.dto.PositionDto;
 
+import lombok.extern.log4j.Log4j2;
+
 @Aspect
 @Component
+@Log4j2
 public class PositionAspect {
-private static final Logger LOGGER = LogManager.getLogger(PositionAspect.class);
+//	private static final Logger LOGGER = LoggerFactory.getLogger(PositionAspect.class);
 	
 	@AfterReturning(value="execution(* com.demo.medium.serviceimpl.PositionServiceImpl.*(..))",returning="position")  
 	public void afterReturningAdvice(JoinPoint joinPoint, PositionDto position)  
 	{  
-	LOGGER.info("After Returing method: "+joinPoint.getSignature());  
-	LOGGER.info(position);  
+	log.info("After Returing method: "+joinPoint.getSignature());  
+	log.info("{}", position);  // SLF4J, Log4j
 	}
 	
 	
 	@AfterReturning(value="execution(* com.demo.medium.serviceimpl.PositionServiceImpl.getAll*(..))",returning="position")  
 	public void afterReturningAdvice(JoinPoint joinPoint, List<PositionDto> position)  
 	{  
-	LOGGER.info("After Returing method: "+joinPoint.getSignature());  
-	LOGGER.info(position);  
+	log.info("After Returing method : {}",joinPoint.getSignature());
+	log.info("{}", position);  // SLF4J, Log4j
 	}
 }
